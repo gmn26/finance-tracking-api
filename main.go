0d55c0479078608794a1d5f8ba46aa5f8ce0b9f7
gmn26 <ghazynovrial@gmail.com>
2025-01-01 11:50:34 +0700

    update : implement transaction process

--- main.go
+++ main.go
@@ -1,19 +1,44 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/gmn26/finance-tracking-api/config"
+	"github.com/gmn26/finance-tracking-api/controller"
+	"github.com/gmn26/finance-tracking-api/model"
+	"github.com/gmn26/finance-tracking-api/repository"
+	"github.com/gmn26/finance-tracking-api/router"
+	"github.com/gmn26/finance-tracking-api/service"
+	"github.com/go-playground/validator/v10"
 )
 
 func main() {
 	gin.SetMode(gin.ReleaseMode)
 
-	router := gin.Default()
+	db := config.DatabaseConnection()
+	validate := validator.New()
 
-	router.GET("/", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, "Testing")
-	})
+	db.Table("transactions").AutoMigrate(&model.Transaction{})
 
-	router.Run("localhost:6969")
+	transactionRepository := repository.TransactionRepositoryImpl(db)
+
+	transactionService := service.TransactionRepositoryImpl(transactionRepository, validate)
+
+	transactionController := controller.NewTransactionController(transactionService)
+
+	routes := router.TransactionRouter(transactionController)
+
+	server := &http.Server{
+		Addr:           ":6969",
+		Handler:        routes,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	err := server.ListenAndServe()
+	log.Fatal(err)
 }
